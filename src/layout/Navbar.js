33d06d4c26f121f4183a0f56f3580f9f739c65f2
OrdnasselaOttogim <alessandro.migotto@studenti.unitn.it>
2023-01-16 16:45:18 +0100

    now oauth service works with docker-compose

--- src/layout/Navbar.js
+++ src/layout/Navbar.js
@@ -3,6 +3,11 @@ import { Link } from 'react-router-dom'
 
 export default function Navbar() {
 
+    require('dotenv').config();
+
+    const oauthUrl = process.env.REACT_APP_OAUTH_SERVICE_URL;
+    console.log("oauthurl=" + oauthUrl);
+
     return (
         <div>
             <nav className="navbar navbar-expand-lg navbar-dark bg-primary">
@@ -14,7 +19,7 @@ export default function Navbar() {
 
                     <Link className='btn btn-outline-light' to="/login">Login</Link>
                     <Link className='btn btn-outline-light' to="/register">Register</Link>
-                    <a className='btn btn-outline-light' href="http://localhost:8080">RegisterGoogle</a>
+                    <a className='btn btn-outline-light' href={oauthUrl}>RegisterGoogle</a>
                     <Link className='btn btn-outline-light' to="/addjob">AddJob</Link>
                 </div>
             </nav>
